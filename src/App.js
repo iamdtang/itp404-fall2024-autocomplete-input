9b7ed6e7cd86b1a14e387874afd2d5d6f076f3e3
David Tang <687449+iamdtang@users.noreply.github.com>
2024-10-06 22:12:32 -0700

    A static autocomplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" list="fruits" />
+
+      <datalist id="fruits">
+        <option value="Apple" />
+        <option value="Blueberry" />
+        <option value="Pear" />
+        <option value="Strawberry" />
+      </datalist>
     </div>
   );
 }
-
-export default App;
