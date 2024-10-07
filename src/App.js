5e98e5ea454df77fb5c2d7bcf5ffec5f1917efca
David Tang <687449+iamdtang@users.noreply.github.com>
2024-10-06 22:31:01 -0700

    Autocomplete for the GitHub repos API without debouncing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import { useState } from "react";
 
+function fetchRepos(search) {
+  return fetch(`https://api.github.com/search/repositories?q=${search}`, {
+    headers: {
+      // https://github.com/settings/tokens
+      Authorization: process.env.REACT_APP_GITHUB_API_TOKEN,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      return json.items.map((repo) => {
+        return repo.full_name;
+      });
+    });
+}
+
 export default function App() {
   const [search, setSearch] = useState("");
+  const [options, setOptions] = useState([]);
 
   return (
     <div>
       <input
         type="text"
-        list="fruits"
+        list="repos"
         value={search}
         onChange={(event) => {
           const value = event.target.value;
 
           setSearch(value);
+
+          if (value) {
+            fetchRepos(value).then((repos) => {
+              setOptions(repos);
+            });
+          } else {
+            setOptions([]);
+          }
         }}
       />
 
-      <datalist id="fruits">
-        <option value="Apple" />
-        <option value="Blueberry" />
-        <option value="Pear" />
-        <option value="Strawberry" />
+      <datalist id="repos">
+        {options.map((option) => {
+          return <option key={option} value={option} />;
+        })}
       </datalist>
     </div>
   );
